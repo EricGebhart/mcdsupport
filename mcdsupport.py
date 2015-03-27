b8309bacc5c873a88cc573aec8d2c684b8a1e082
Oliver P. Mayor <oliver@olimay.net>
2015-03-27 02:29:53 -0400

    Update code headers with new repo URL and maintainer info.

--- mcdsupport.py
+++ mcdsupport.py
@@ -5,7 +5,10 @@
 #
 # License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 #
-# This project is hosted on GitHub: https://github.com/tarix/mcdsupport
+# This project is hosted on GitHub: https://github.com/olimay/mcdsupport
+#
+# Addon by mikem (tarix).
+# Maintained by Oliver Mayor (olimay).
 
 #from aqt import mw
 #from anki.hooks import addHook
